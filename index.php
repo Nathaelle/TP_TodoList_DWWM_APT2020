6b8a4999517357e97507e912209333aa104b9d64
Nathaelle <scriptales.clients@gmail.com>
2020-04-10 09:42:25 +0200

    Plan pour l'appel de nos fonctions de contrôle

--- index.php
+++ index.php
@@ -11,14 +11,30 @@ require_once "models/Utilisateur.php";
 // 2. ROUTER
 // Structure permettant d'appeler une action en fonction de la requête utilisateur
 
-// ........
+$route = isset($_POST["route"])? $_POST["route"] : "home";
+
+switch($route) {
+    case "home" : showHome();
+        break;
+    case "insert_user" : insertUser();
+        break;
+    default : showHome();
+}
 
 // ------------------------------------------------------------------------------------
 // 3. FONCTIONS DE CONTROLE
 // Actions déclenchées en fonction du choix de l'utilisateur
-// 1 choix = 1 fonction
+// 1 choix = 1 fonction avec deux "types" de fonctions, celles qui mèneront à un affichage, et celles qui seront redirigées (vers un choix conduisant à un affichage)
 
-// ........
+// Fonctionnalité(s) d'affichage :
+function showHome() {
+
+}
+
+// Fonctionnalité(s) redirigées :
+function insertUser() {
+
+}
 
 // ------------------------------------------------------------------------------------
 // 4. TEMPLATE
