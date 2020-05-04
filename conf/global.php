93b9a37c930a0b0d023373516bcf71121f62fbe4
Nathaelle <scriptales.clients@gmail.com>
2020-05-04 11:22:27 +0200

    Info importante

--- conf/global.php
+++ conf/global.php
@@ -1,4 +1,7 @@
 <?php
+// Je vous envoie les définitions pour exemple, mais on ne versionne JAMAIS JAMAIS JAMAIS les informations de connexion à la base de données !!!!!
+// -> .gitignore
+
 define ('DATABASE', 'mysql:host=localhost;dbname=todonew;charset=utf8');
 define ('LOGIN', 'root');
 define ('PASSWD', '');
