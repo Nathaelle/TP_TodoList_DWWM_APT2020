ca95387c1e061c385dd5875edbb70848c10e3954
Nathaelle <scriptales.clients@gmail.com>
2020-06-02 08:46:01 +0200

    Ajout bdd

--- bdd.sql
+++ bdd.sql
@@ -1,32 +1,75 @@
-# ------------------------------------------------------------
-#        Script MySQL.
-#------------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3308
+-- Généré le :  mar. 02 juin 2020 à 06:41
+-- Version du serveur :  8.0.18
+-- Version de PHP :  7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-#------------------------------------------------------------
-# Table: Users
-#------------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE Users(
-	id_user INT AUTO_INCREMENT NOT NULL,
-	nom VARCHAR(70) NOT NULL,
-	prenom VARCHAR(70) NOT NULL,
-	email VARCHAR(70) UNIQUE NOT NULL,
-	pseudo VARCHAR(70) UNIQUE NOT NULL,
-	password VARCHAR(70) NOT NULL,
-	CONSTRAINT users_pk PRIMARY KEY (id_user)
-)ENGINE=InnoDB;
+--
+-- Base de données :  `todonew`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id_task` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `deadline` datetime NOT NULL,
+  `id_user` int(11) NOT NULL,
+  PRIMARY KEY (`id_task`),
+  KEY `tasks_users_fk` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-#------------------------------------------------------------
-# Table: Tasks
-#------------------------------------------------------------
+-- --------------------------------------------------------
 
-CREATE TABLE Tasks(
-        id_task INT Auto_increment NOT NULL,
-        description VARCHAR (255) NOT NULL,
-        deadline DATETIME NOT NULL,
-        id_user INT NOT NULL,
-        CONSTRAINT tasks_pk PRIMARY KEY (id_task),
-        CONSTRAINT tasks_users_fk FOREIGN KEY (id_user) REFERENCES Users(id_user)
-)ENGINE=InnoDB;
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(70) NOT NULL,
+  `prenom` varchar(70) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `pseudo` varchar(70) NOT NULL,
+  `password` varchar(70) NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `pseudo` (`pseudo`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_users_fk` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
